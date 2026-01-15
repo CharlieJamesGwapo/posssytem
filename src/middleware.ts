f157172f39b71678845bac00806a2b9df527c800
CharlieJames <capstonee2@gmail.com>
2026-01-15 10:34:07 +0800

    NEW UPDATE BACKENDD

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl
-
-  // Protect staff routes (except login)
-  if (pathname.startsWith('/staff') && pathname !== '/staff-login') {
-    // Check for staff token in cookies
-    const token = request.cookies.get('staffToken')?.value
-
-    // If no token, redirect to staff login
-    if (!token) {
-      return NextResponse.redirect(new URL('/staff-login', request.url))
-    }
-  }
-
-  // Redirect /staff-login to /staff if already authenticated
-  if (pathname === '/staff-login') {
-    const token = request.cookies.get('staffToken')?.value
-    if (token) {
-      return NextResponse.redirect(new URL('/staff', request.url))
-    }
-  }
-
+  // Temporarily disable all middleware to fix redirect loop
   return NextResponse.next()
 }
 
